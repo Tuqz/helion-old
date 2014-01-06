d768f0902453fc228c47a6457add59225c53e124
Tuqz <jamesrawson628@gmail.com>
2014-01-06 17:57:20 +0000

    Removed old files

--- src/part.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace sim {
-	struct Part {
-		enum {control, tank, engine} type;
-		char* contents; //For example electricity for a control, fuel for tank...
-		double quantity;
-	};
-}
